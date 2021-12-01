c3020c9b6e2302509bf30b238fe986055d58c572
Dylan Neve <dylanneve1@gmail.com>
2021-12-01 22:01:04 +0000

    bullhead: Build android.hardware.power@1.0
    * E init: Control message: Could not find 'android.hardware.power@1.0::IPower/default' for ctl.interface_start

--- device.mk
+++ device.mk
@@ -334,6 +334,7 @@ PRODUCT_COPY_FILES += \
 # Power HAL
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    android.hardware.power@1.0 \
     android.hardware.power.stats@1.0-service.mock
 
 # Power configuration file
