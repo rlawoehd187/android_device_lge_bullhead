89c6057501f0cff36fe760347ec9ead24dc68a26
Bruno Martins <bgcngm@gmail.com>
2022-03-15 20:53:47 +0000

    bullhead: Copy cgroups and task_profiles directly from system/core
    * This ensures that the most up-to-date JSON files are shipped.
    
    Change-Id: Ic9fae188784825ae6dc607c13b403e37290b7067

--- device.mk
+++ device.mk
@@ -152,6 +152,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Control groups and task profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8992 \
