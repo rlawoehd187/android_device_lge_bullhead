e440c7b0af2185ebf7e1498d97fff5933907bdd1
hiru <rlawoehd187@naver.com>
2023-03-11 22:15:02 +0900

    bullhead: Convert all shims to blue print
    Signed-off-by: hiru <rlawoehd187@naver.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,4 +1,5 @@
-# Copyright 2010 The Android Open Source Project
+#
+# Copyright (C) 2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,25 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH:= $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := libaudioclient_shim.cpp
-LOCAL_MODULE := libaudioclient_shim
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_SHARED_LIBRARIES := libaudioclient
-LOCAL_VENDOR_MODULE := true
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := SensorManager.cpp
-LOCAL_MODULE := libshims_sensormanager
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_VENDOR_MODULE := true
-LOCAL_CFLAGS_arm64 := -DLIBSHIMS_64BIT
-include $(BUILD_SHARED_LIBRARY)
-
+include $(call first-makefiles-under,$(call my-dir))
